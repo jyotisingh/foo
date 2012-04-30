578ed12f6625374b36ff26f98ef40914ab70de37
Jyoti Singh <jsingh@thoughtworks.com>
2012-04-30 22:39:55 +0530

    edit assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -12,11 +12,24 @@ class AssetsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @asset = Asset.new(params[:asset].merge({:fund => Fund.first}))
-    @asset.fund = Fund.first
     @user.assets << @asset
     @user.valid?
     render :new and return unless @user.errors.empty?
     @user.save
+    redirect_to user_assets_path
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @asset = @user.assets.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @asset = @user.assets.find(params[:id])
+    @asset.update_attributes(params[:asset].merge({:fund => Fund.first}))
+    @asset.valid?
+    render :edit and return unless @asset.errors.empty?
     redirect_to user_assets_path
   end
 end
--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -26,4 +26,25 @@ describe AssetsController do
     assigns(:user).should == user
     assigns(:assets).should == user.assets
   end
+
+  it "should edit assets for user" do
+    user = User.new(:name => "a", :email => "a@aa.com")
+    user.assets << Asset.new(:quantity_purchased => 10, :book_value => 10.0)
+    user.save
+    get :edit, :user_id => user.id, :id => user.assets.first.id
+    assigns(:user).should == user
+    assigns(:asset).should == user.assets.first
+    response.should be_success
+    response.should render_template :edit
+  end
+
+  it "should update asset" do
+    user = User.new(:name => "a", :email => "a@aa.com")
+    user.assets << Asset.new(:quantity_purchased => 10, :book_value => 10.0)
+    user.save
+    put :update, :user_id => user.id,:id => user.assets[0].id, :asset => {:quantity_purchased => 100, :book_value => 10.0}
+    response.should redirect_to user_assets_path(user) 
+    user.reload
+    user.assets.first.quantity_purchased.should == 100
+  end
 end
